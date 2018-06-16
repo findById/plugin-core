ec7523d5a75dd6e72935782cdda714d1c663b14b
chenning <chenning@doweidu.com>
2018-06-16 23:20:46 +0800

    Initial commit

--- src/main/java/org/cn/plugin/Plugin.java
+++ src/main/java/org/cn/plugin/Plugin.java
@@ -4,6 +4,13 @@ package org.cn.plugin;
  * Created by chenning on 2016/12/29.
  */
 public interface Plugin {
-    void onCreate();
-    void onDestroy();
+    default void onCreate() {
+    }
+
+    default void onDestroy() {
+    }
+
+    default PluginStatus status() {
+        return PluginStatus.RUNNING;
+    }
 }
